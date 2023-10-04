e5e1b01a24bf01a1a541e769350cc226abb5fc30
Narayana <veera.v@trundl.com>
2023-10-04 16:09:38 +0530

    Artifact

---
+++ artifact.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+cd /home/circleci/
+mkdir -p artifacts
+pwd
+cp -r /home/circleci/Flask-calculator /home/circleci/artifacts
+cd /home/circleci/artifacts
+zip -r Flask-calculator.zip /home/circleci/artifacts/Flask-calculator/*
+chmod 777 Flask-calculator.zip
+ls -la
